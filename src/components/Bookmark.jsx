2f51e2e3034fae25f9facb8ac4765d6eb4d4b5d3
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2018-08-03 10:00:46 +1000

    Bumped up the font size of the alias text

--- src/components/Bookmark.jsx
+++ src/components/Bookmark.jsx
@@ -3,21 +3,39 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import {withStyles} from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography/Typography';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
 import AliasProp from '../utils/AliasProp';
 
-const styles = () => ({
+const styles = theme => ({
 	root: {},
+	alias: {
+		fontSize: theme.typography.pxToRem(16),
+	},
+	href: {
+		fontSize: 'inherit',
+	},
 });
 
 // todo: https://material.io/guidelines/components/lists.html#lists-usage
-
 const Bookmark = ({alias, classes, className, ...other}) => (
 	<ListItem button className={classNames(classes.root, className)} {...other}>
-		<ListItemText primary={alias.alias} secondary={alias.href} />
+		<ListItemText
+			disableTypography
+			primary={
+				<Typography variant="subheading" component="span" className={classes.alias}>
+					{alias.alias}
+				</Typography>
+			}
+			secondary={
+				<Typography variant="body1" component="span" color="textSecondary" className={classes.href}>
+					{alias.href}
+				</Typography>
+			}
+		/>
 		<ListItemSecondaryAction>
 			<IconButton>
 				<MoreVertIcon />
