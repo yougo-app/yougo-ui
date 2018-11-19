df44207b2904e9350217e4ef906370929a5ee9b8
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2018-11-19 16:14:41 +1100

    Fixed entities reducer merge so that state is updated correctly

--- src/reducers/entities.js
+++ src/reducers/entities.js
@@ -3,5 +3,5 @@ import {prefix} from '../constants/actionTypes';
 
 export default (state = {}, {type, payload}) =>
 	type.startsWith(prefix) && payload && 'entities' in payload
-		? merge(state, payload.entities)
+		? merge({}, state, payload.entities)
 		: state;
