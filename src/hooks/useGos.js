f35d6f54499a3e56f1a4f1234eeb5248aff78c31
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2020-08-23 20:32:58 +0100

    supply access token with api requests

--- src/hooks/useGos.js
+++ src/hooks/useGos.js
@@ -1,6 +1,10 @@
+import axios from 'axios';
 import {useQuery} from 'react-query';
 import goApi from 'util/goApi';
 
+import useAuthHeader from './useAuthHeader';
+
 export default function useGos() {
-	return useQuery(goApi.gos);
+	const config = useAuthHeader();
+	return useQuery(goApi.gos, (path) => axios.get(path, config).then((response) => response.data));
 }
--- src/util/queryConfig.js
+++ src/util/queryConfig.js
@@ -1,18 +1,10 @@
 import axios from 'axios';
 import {API_URL, CACHE_STALE_TIME} from 'util/env';
-import joinURL from 'util/joinUrl';
 
 axios.defaults.baseURL = API_URL;
 
-export const fetcher = (endpoint, segments = undefined, options = undefined) => {
-	return axios
-		.get(joinURL(endpoint, ...[segments].flat()), options)
-		.then((response) => response.data);
-};
-
 const queryConfig = Object.freeze({
 	queries: {
-		queryFn: fetcher,
 		staleTime: CACHE_STALE_TIME,
 	},
 });
