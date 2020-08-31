670d25b28fd9d96fb15aa24e63169345fbabc019
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2020-08-31 14:57:09 +0100

    autologin by default

--- src/util/env.js
+++ src/util/env.js
@@ -15,5 +15,5 @@ export const OIDC_AUTHORITY = env('OIDC_AUTHORITY', 'https://accounts.google.com
 export const OIDC_CLIENT_ID = env('OIDC_CLIENT_ID');
 export const OIDC_CLIENT_SECRET = env('OIDC_CLIENT_SECRET');
 export const OIDC_REDIRECT_URI = env('OIDC_REDIRECT_URI', 'http://localhost:3000');
-export const OIDC_AUTO_SIGNIN = bool(env('OIDC_AUTO_SIGNIN', 'false'));
+export const OIDC_AUTO_SIGNIN = bool(env('OIDC_AUTO_SIGNIN', 'true'));
 export const CACHE_STALE_TIME = parseInt(env('CACHE_STALE_TIME', '60000'), 10);
