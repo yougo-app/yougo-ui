c19d8e628ad11b6e8184d1dbe676847e9982dc25
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2021-09-17 17:12:54 +0100

    Remove StyleEngineProvider

--- src/components/Providers/Providers.js
+++ src/components/Providers/Providers.js
@@ -1,6 +1,6 @@
 import {Auth0Provider} from '@auth0/auth0-react';
 import {CssBaseline} from '@mui/material';
-import {StyledEngineProvider, ThemeProvider} from '@mui/material/styles';
+import {ThemeProvider} from '@mui/material/styles';
 import {SearchProvider} from 'context/SearchContext';
 import {SnackbarProvider} from 'notistack';
 import {node} from 'prop-types';
@@ -12,20 +12,18 @@ import theme from 'util/theme';
 
 const Providers = ({children}) => (
 	<Auth0Provider {...authConfig}>
-		<StyledEngineProvider injectFirst>
-			<ThemeProvider theme={theme}>
-				<QueryClientProvider client={queryClient}>
-					<SnackbarProvider maxSnack={3}>
-						<ModalProvider>
-							<SearchProvider>
-								<CssBaseline />
-								{children}
-							</SearchProvider>
-						</ModalProvider>
-					</SnackbarProvider>
-				</QueryClientProvider>
-			</ThemeProvider>
-		</StyledEngineProvider>
+		<ThemeProvider theme={theme}>
+			<QueryClientProvider client={queryClient}>
+				<SnackbarProvider maxSnack={3}>
+					<ModalProvider>
+						<SearchProvider>
+							<CssBaseline />
+							{children}
+						</SearchProvider>
+					</ModalProvider>
+				</SnackbarProvider>
+			</QueryClientProvider>
+		</ThemeProvider>
 	</Auth0Provider>
 );
 
